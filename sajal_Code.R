f89a42521410232b152a852043a6b4b1db648bd5
Sultan Mahmud Sajal <sxs2561@psu.edu>
2019-02-06 19:37:12 -0500

    done with logspace arithmatic

---
+++ sajal_Code.R
@@ -0,0 +1,56 @@
+##################################
+###### STAT 557 (Project 1) ######
+##################################
+
+rm(list=ls()) ## To clear your environment
+
+## Read the data
+xTrain=read.csv("ecoli_xTrain.csv")
+yTrain=read.csv("ecoli_yTrain.csv")
+xTest=read.csv("ecoli_xTest.csv")
+yTest=read.csv("ecoli_yTest.csv")
+
+
+
+#### Part 1 ####
+logProd <- function(x){
+    
+    logProduct=0
+    for(i in 1:length(x)){
+        logProduct=logProduct+x[i]
+    }
+    
+    return(logProduct)
+}
+
+logSum <- function(x){
+    
+    # use logsum function from https://www.rdocumentation.org/packages/tileHMM/versions/1.0-7/topics/logSum
+    # logSummation=logSum(x,y=null,base=0);
+    
+    sortedX = sort(x, decreasing = TRUE)
+    print(sortedX)
+    
+    x_1 = x[1]
+    
+    logSecondTerm=0
+    for(i in 2:length(x)){
+        
+        logSecondTerm = logSecondTerm + exp(x[i]-x_1)
+    
+    }
+    
+    logTerm=1+logSecondTerm
+    print("ln(5) = ",log(5))
+    logSummation=x_1+log(logTerm)
+    
+    return(logSummation)
+}
+
+
+testingX=c(0,1.609437,1.386294,2.1972245,2.3978952)
+productOfX=logProd(testingX)
+print(productOfX)
+
+sumOfX=logSum(testingX)
+print(sumOfX)
